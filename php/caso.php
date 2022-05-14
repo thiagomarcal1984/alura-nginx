563fd2540d8190f9d6dabadc50c04b60c474a9f5
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-05-14 11:48:00 -0300

    Redirecionando requisições PHP para o servidor de aplicação.

---
+++ php/caso.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "<h1>Caso.php</h1>";
+echo "<p>
+    Dependendo da configuração da RegEx no nginx, o caminho da URL 
+    precisa combinar com o da RegEx, senão dá erro.
+</p>";
---
+++ php/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1>Index Gerada em PHP!</h1>";
+
+echo "<p>Repare que o link vai pra página CASO.php:</p>";
+
+echo '<p><a href="caso.php">caso.php</a></p>';
+echo '<p><a href="CASO.PHP">CASO.PHP</a></p>';
